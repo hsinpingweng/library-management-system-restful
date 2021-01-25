5c629ed7ca5a204e8bd02b589e0a6ea056821373
wenghsinping <ohmyhappy77@gmail.com>
2021-01-25 17:35:43 +0000

    init project

---
+++ src/main/java/com/hsinpingweng/library/librarymanagementsystemrestful/LibraryManagementSystemRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.hsinpingweng.library.librarymanagementsystemrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsinpingweng/library/librarymanagementsystemrestful/LibraryManagementSystemRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsinpingweng.library.librarymanagementsystemrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
